e5db7e8aefab65d131af8d57e881430427c41790
UbuntuDell2 <nathan.chevalier@epita.fr>
2022-02-24 10:11:26 +0100

    Added ref, comments for student file

--- client/student.py
+++ client/student.py
@@ -9,26 +9,25 @@ import my_little_chat
 
 
 def main() -> None:
-    name = input("Choose your name:")
-    address = input("Server ip:")
-    port = input("Server port:")
+    # TODO
 
-    connection = Connection(name, address, port)
+    # Recupérez les informations nécéssaires avec input(), puis créez une nouvelle connection
+    # nom
+    # addresse ip
+    # port de serveur
 
-    while True:
-        event = connection.waitEvent()
 
-        if (event == EventType.READ):
-            connection.readInput()
-        elif (event == EventType.RECV):
-            msg = connection.receive
-            print(msg)
-        elif (event == EventType.SEND):
-            connection.send()
-        else:
-            print("Error: Bad Event Type: #TATOUKC")
-        
-    connection.close()
+    # Dans une boucle infinie, attendez les nouveaux evenements
+    # Appelez la fonction correspondante selon le type d'evenement (READ, RECV, SEND)
+
+
+
+
+
+
+
+    # N'oubliez pas de fermer votre connection
+
 
 if __name__ == "__main__":
     main()
